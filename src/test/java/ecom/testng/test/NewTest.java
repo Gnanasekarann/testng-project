545949ef5585073d5f6b17659caa42abacaa0e42
Gnanasekarann <85561299+Gnanasekarann@users.noreply.github.com>
2021-07-29 21:38:55 +0530

    Update NewTest.java

--- src/test/java/ecom/testng/test/NewTest.java
+++ src/test/java/ecom/testng/test/NewTest.java
@@ -87,7 +87,7 @@ public class NewTest extends BaseElements {
 		webElement.sendKeys("1000");
 		driver.findElement(By.linkText("Update")).click();
 		driver.findElement(By.linkText("Empty Cart")).click();
-	  	driver.quit();
+	  	driver.close();
 }
  
 }
